nitrogen-project/android_vendor_nitrogen

prebuilt: Add a script to simplify A-only recovery system  mount
* This can be helpful because of the fragmentation that will happen with the system mount in recovery after Q

products/common.mk
@@ -93,7 +93,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/nitrogen/prebuilt/common/addon.d/50-nitrogen.sh:system/addon.d/50-nitrogen.sh \
     vendor/nitrogen/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/nitrogen/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
+    vendor/nitrogen/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/nitrogen/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
 
 # Priv-app config
 PRODUCT_COPY_FILES += \

pxn-wip/vendor_nitrogen

Signed-off-by: drpradsms <dr.pradsms@gmail.com>

products/common.mk
@@ -101,7 +101,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/pixen/prebuilt/common/addon.d/50-pixen.sh:system/addon.d/50-pixen.sh \
     vendor/pixen/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/pixen/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
+    vendor/pixen/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/pixen/prebuilt/common/bin/system-mount.sh:install/bin/system-mount.sh
 
 # Priv-app config
 PRODUCT_COPY_FILES += \